a2961f564896950fc5faf1aef61edc3095d0218f
vvpgbd <118769412+vvpgbd@users.noreply.github.com>
2022-12-01 19:10:06 +0530

    Add files via upload

---
+++ STRR.js
@@ -0,0 +1,28 @@
+function sc(){
+
+    let pwd = document.querySelector('#pwd').value
+    let pw = pwd.length
+    let medium_pwd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})$/
+    let strong_pwd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})$/
+
+    if(pw <= 1){
+        document.querySelector('.em').innerText = 'password must be not be blank'
+
+    }else if(''){
+        document.querySelector('.em').innerText = 'Weak'
+    } 
+    
+    else if(pwd.match(medium_pwd)){   
+         document.querySelector('.sm').innerText = 'medium'
+
+    } else if(!pwd.match(strong_pwd)){
+        console.log('strong ')
+        document.querySelector('.em').innerText = 'pwds must contain 1 no,1 upper case cahr,1 special char'
+    } else{
+
+        console.log('strong working')
+        document.querySelector('.em').innerText = ''
+        document.querySelector('.sm').innerText = 'Strong'
+    }
+
+}
